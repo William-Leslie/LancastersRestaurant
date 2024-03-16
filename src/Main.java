e9738e421cda910700a307e8468bb5ba736fd313
William-Leslie <127599477+William-Leslie@users.noreply.github.com>
2024-03-16 17:30:45 +0000

    Restructure Main class and add comments

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,40 @@
-import Screens.Screen;
+import Screens.*;
 
 import java.awt.*;
 import javax.swing.*;
 
 public class Main extends JFrame {
-    private JPanel screens = new JPanel(new CardLayout());
-
     public Main() {
+        // Initialize window with title
         super("Lancaster's Restaurant");
 
-        this.screens.add(new Screens.Index(this.screens), Screen.Index.name());
-        this.screens.add(new Screens.Login(this.screens), Screen.Login.name());
-        this.screens.add(new Screens.Selection(this.screens), Screen.Selection.name());
-        this.screens.add(new Screens.Home(this.screens), Screen.Home.name());
+        // Screens contained in a CardLayout manager, must be passed to each screen to be able to change active screen
+        JPanel screens = new JPanel(new CardLayout());
 
-        CardLayout cl = (CardLayout)this.screens.getLayout();
-        cl.show(this.screens, Screen.Index.name());
+        // Create all screens and pass screens manager reference, each screen is identified by Screen enum name
+        screens.add(new Screens.Index(screens), Screen.Index.name());
+        screens.add(new Screens.Login(screens), Screen.Login.name());
+        screens.add(new Screens.Selection(screens), Screen.Selection.name());
+        screens.add(new Screens.Home(screens), Screen.Home.name());
 
-        this.add(this.screens);
+        // Show initial screen
+        CardLayout cl = (CardLayout) screens.getLayout();
+        cl.show(screens, Screen.Index.name());
+
+        // Add screens manager to main window
+        this.add(screens);
         this.pack();
+
+        // Set main window configuration
         this.setLocationRelativeTo(null);
         this.setMinimumSize(new Dimension(800, 800));
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
     public static void main(String[] args) {
-        new Main().setVisible(true);
+        // Create and show window in event loop
+        SwingUtilities.invokeLater(() -> {
+            new Main().setVisible(true);
+        });
     }
 }
