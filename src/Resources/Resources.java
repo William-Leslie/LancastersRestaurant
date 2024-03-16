ec62ebec632a8f78ffa351bf08f38e2baf093fb6
William-Leslie <127599477+William-Leslie@users.noreply.github.com>
2024-03-16 16:30:41 +0000

    Improve logo loading and add font loader

--- src/Resources/Resources.java
+++ src/Resources/Resources.java
@@ -7,17 +7,34 @@ import java.awt.image.*;
 import java.io.*;
 
 public class Resources {
+    private static BufferedImage logo = null;
+    private static Font font = null;
+
     public static JLabel getLogo(int width, int height) {
-        JLabel logo = null;
         try {
-            BufferedImage image = ImageIO.read(new File("resources/LancastersLogo.jpeg"));
-            Image resized = image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
-            logo = new JLabel(new ImageIcon(resized));
+            if(Resources.logo == null) {
+                Resources.logo = ImageIO.read(new File("resources/LancastersLogo.jpeg"));
+            }
         } catch (IOException exception) {
-            logo = new JLabel("Logo");
+            exception.printStackTrace();
+            Resources.logo = new BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB);
         }
+        Image resized = Resources.logo.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        JLabel logo = new JLabel(new ImageIcon(resized));
         logo.setSize(new Dimension(width, height));
         logo.setBorder(BorderFactory.createMatteBorder(2, 2, 2, 2, new Color(0xffffff)));
         return logo;
+    }
+
+    public static Font getFont(float size) {
+        try {
+            if(Resources.font == null) {
+                Resources.font = Font.createFont(Font.TRUETYPE_FONT, new File("resources/AbrilFatface-Regular.ttf"));
+            }
+        } catch (IOException | FontFormatException exception) {
+            exception.printStackTrace();
+            Resources.font = new Font("serif", Font.PLAIN, 20);
+        }
+        return Resources.font.deriveFont(size);
     }
 }
