2d6c5644e039fc9a0ff689474c8b56c6c4866636
William-Leslie <127599477+William-Leslie@users.noreply.github.com>
2024-04-02 13:27:48 +0100

    Tidy up Inventory a bit (still needs work)

--- src/Screens/Inventory.java
+++ src/Screens/Inventory.java
@@ -26,61 +26,40 @@ public class Inventory extends JPanel {
         this.add(navbar, constraints);
 
         // FIXME: Dummy data
-        String[][] rec = {
-            {"1", "Steve", "AUS"},
-            {"2", "Virat", "IND"},
-            {"3", "Kane", "NZ"},
+        String[][] ingredientsData = {
+            {"Milk", "10", "20"},
+            {"Egg", "20", "15"},
+            {"Flour", "30", "10"},
         };
-        String[] header = {"IngredientID", "Name", "StockLevel"};
-        JTable inventoryTable = new JTable(rec, header);
-        inventoryTable.setRowHeight(30);
-        JScrollPane selection = new JScrollPane(inventoryTable);
+        String[] IngredientsHeader = {"Ingredient", "In Stock", "Minimum Stock"};
 
         // TODO: Theming JTable
-        inventoryTable.setForeground(new Color(0x2b3336));
-        // inventoryTable.setBackground(new Color(0x455a61));
-        selection.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, new Color(0xaaaaaa)));
-        inventoryTable.setFont(Resources.getFont(15));
+        JTable tableInventory = new JTable(ingredientsData, IngredientsHeader);
+        tableInventory.setForeground(new Color(0x2b3336));
+        tableInventory.setRowHeight(30);
+        tableInventory.setFont(Resources.getFont(20));
+
+        JScrollPane scrollInventory = new JScrollPane(tableInventory);
+        scrollInventory.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, new Color(0xaaaaaa)));
         constraints.gridx = 1;
         constraints.gridy++;
         constraints.weighty = 1;
         constraints.gridwidth = 2;
         constraints.insets = new Insets(10, 10, 0, 10);
-        this.add(selection, constraints);
+        this.add(scrollInventory, constraints);
 
-        String[] inventoryNames = {"ingredient", "dish"};
-
-      /*  JList<String> inventorySelection = new JList<>(inventoryNames);
-        inventorySelection.setForeground(new Color(0x2b3336));
-        inventorySelection.setBackground(new Color(0xffffff));
-        inventorySelection.setBorder(BorderFactory.createMatteBorder(3, 3, 3, 3, new Color(0xaaaaaa)));
-        inventorySelection.setFont(Resources.getFont(18));
-        constraints.gridy++;
-        constraints.fill = GridBagConstraints.BOTH;
-        constraints.anchor = GridBagConstraints.CENTER;
-        constraints.gridheight = 1;
-        constraints.insets = new Insets(10, 0, 0, 0);
-        this.add(inventorySelection,constraints);*/
-
-        JTextField ingredientName = new JTextField(20);
-        ingredientName.setForeground(new Color(0x2b3336));
-        ingredientName.setBackground(new Color(0xffffff));
-        ingredientName.setBorder(BorderFactory.createMatteBorder(3, 3, 3, 3, new Color(0xaaaaaa)));
-        ingredientName.setFont(Resources.getFont(18));
+        CTextField fieldIngredient = new CTextField();
         constraints.gridx = 1;
         constraints.gridy++;
+        constraints.weightx = 6;
         constraints.weighty = 0;
         constraints.gridwidth = 1;
         constraints.insets = new Insets(10, 10, 10, 10);
-        this.add(ingredientName, constraints);
+        this.add(fieldIngredient, constraints);
 
-        JButton buttonAdd = new JButton("Add");
-        buttonAdd.setForeground(new Color(0xcccccc));
-        buttonAdd.setBackground(new Color(0x557b8a));
-        buttonAdd.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, new Color(0xffffff)));
-        buttonAdd.setFont(Resources.getFont(26));
-        buttonAdd.setFocusPainted(false);
+        CButton buttonAdd = new CButton("Add", null);
         constraints.gridx = 2;
+        constraints.weightx = 1;
         constraints.weighty = 0;
         constraints.gridwidth = 1;
         constraints.insets = new Insets(10, 0, 10, 10);
