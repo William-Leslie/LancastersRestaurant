743939a338922df0bf9dda5253f472a0f7c067ba
William-Leslie <William-Leslie@users.noreply.github.com>
2024-04-14 19:29:19 +0100

    Revert "Use "data" database user"
    This reverts commit 719e297654af9ef4ff253446c4f5a0331f702b36.

--- src/Resources/Database.java
+++ src/Resources/Database.java
@@ -4,8 +4,8 @@ import java.sql.*;
 
 public class Database {
     static String url = "jdbc:mysql://smcse-stuproj00.city.ac.uk:3306/in2033t30";
-    static String username = "in2033t30_d";
-    static String password = "Cl3nYGOgp0o";
+    static String username = "in2033t30_a";
+    static String password = "ZSsixpPY6cQ";
 
     public static Connection connection() throws SQLException {
         return DriverManager.getConnection(url, username, password);
