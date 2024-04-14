c03654972b7b7c22935c7d9ec003c9c3354b530d
William-Leslie <William-Leslie@users.noreply.github.com>
2024-04-14 23:27:44 +0100

    Private vars

--- src/Resources/Database.java
+++ src/Resources/Database.java
@@ -3,9 +3,9 @@ package Resources;
 import java.sql.*;
 
 public class Database {
-    static String url = "jdbc:mysql://smcse-stuproj00.city.ac.uk:3306/in2033t30";
-    static String username = "in2033t30_a";
-    static String password = "ZSsixpPY6cQ";
+    private static final String url = "jdbc:mysql://smcse-stuproj00.city.ac.uk:3306/in2033t30";
+    private static final String username = "in2033t30_a";
+    private static final String password = "ZSsixpPY6cQ";
 
     public static Connection connection() throws SQLException {
         return DriverManager.getConnection(url, username, password);
