6a89bec292e05df9dbd9e8993ee6234c5816c01f
Martinson-Yan <martinson.yan@city.ac.uk>
2024-04-15 11:57:22 +0100

    changed color

--- src/Resources/Colors.java
+++ src/Resources/Colors.java
@@ -5,7 +5,7 @@ import java.awt.*;
 public class Colors {
     public static final Color background = new Color(40, 48, 51);
     public static final Color text = new Color(237, 236, 232);
-    public static final Color frame = new Color(0xaaaaaa);
+    public static final Color frame = new Color(239, 237, 236);
     public static final Color primary = new Color(100, 165, 180);
     public static final Color secondary = new Color(0x455a61);
     public static final Color white = new Color(0xffffff);
