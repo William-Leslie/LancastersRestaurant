b75ef4bf63f45918b31bee6e647a0a705920ef83
AHssnCity <127661689+AHssnCity@users.noreply.github.com>
2024-04-15 12:51:00 +0100

    Fix disabled text color

--- src/Components/CPasswordField.java
+++ src/Components/CPasswordField.java
@@ -8,7 +8,7 @@ public class CPasswordField extends JPasswordField {
     public CPasswordField(int textSize) {
         super(0);
         this.setForeground(Colors.background);
-        this.setDisabledTextColor(Colors.frame);
+        this.setDisabledTextColor(Colors.secondary);
         this.setBackground(Colors.white);
         this.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Colors.frame));
         this.setFont(Resources.getFont(textSize));
